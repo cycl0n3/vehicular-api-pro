e23eb73a7e917408976c311d3a3e7dac338dd1a1
Milind <7milind7@gmail.com>
2023-04-24 00:17:09 +0530

    UserController V1 is done

--- src/main/groovy/com/app/orderapi/rest/UserV1Controller.groovy
+++ src/main/groovy/com/app/orderapi/rest/UserV1Controller.groovy
@@ -1,7 +1,7 @@
 package com.app.orderapi.rest
 
 import com.app.orderapi.mapper.UserMapper
-import com.app.orderapi.model.User
+
 import com.app.orderapi.rest.dto.UserDto
 
 import com.app.orderapi.security.CustomUserDetails
@@ -9,19 +9,24 @@ import com.app.orderapi.security.CustomUserDetails
 import com.app.orderapi.service.UserService
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.domain.Page
+
 import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Pageable
+
 import org.springframework.http.ResponseEntity
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
 
-import java.util.stream.Collectors
+import org.springframework.web.multipart.MultipartFile
+
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -56,5 +61,36 @@ class UserV1Controller {
         response["users"] = pageResult.collect(userMapper::toUserDto)
 
         return ResponseEntity.ok(response)
+    }
+
+    @GetMapping("/{username}")
+    ResponseEntity<UserDto> getUser(@PathVariable String username) {
+        def userDto = userMapper.toUserDto(userService.validateAndGetUserByUsername(username))
+
+        return ResponseEntity.ok(userDto)
+    }
+
+    @DeleteMapping("/{username}")
+    ResponseEntity<UserDto> deleteUser(@PathVariable String username) {
+        def user = userService.validateAndGetUserByUsername(username)
+        userService.deleteUser(user)
+
+        return ResponseEntity.ok(userMapper.toUserDto(user))
+    }
+
+    @PostMapping("/profile-picture")
+    ResponseEntity<Void> uploadProfilePicture(
+        @RequestPart("profile-picture") MultipartFile file,
+        @AuthenticationPrincipal CustomUserDetails currentUser) {
+        try {
+            def user = userService.validateAndGetUserByUsername(currentUser.username)
+            user.profilePicture = file.bytes
+
+            userService.saveUser(user)
+
+            return ResponseEntity.ok().build()
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build()
+        }
     }
 }
