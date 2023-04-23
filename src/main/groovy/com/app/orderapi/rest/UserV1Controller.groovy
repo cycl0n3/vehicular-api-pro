714bcd0b6cf8a4bf36aac3838421bd3f917f0753
Milind <7milind7@gmail.com>
2023-04-24 00:13:05 +0530

    UserController getUsers V1

--- src/main/groovy/com/app/orderapi/rest/UserV1Controller.groovy
+++ src/main/groovy/com/app/orderapi/rest/UserV1Controller.groovy
@@ -1,15 +1,27 @@
 package com.app.orderapi.rest
 
 import com.app.orderapi.mapper.UserMapper
+import com.app.orderapi.model.User
 import com.app.orderapi.rest.dto.UserDto
+
 import com.app.orderapi.security.CustomUserDetails
+
 import com.app.orderapi.service.UserService
+
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.http.ResponseEntity
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -25,5 +37,24 @@ class UserV1Controller {
     ResponseEntity<UserDto> getCurrentUser(@AuthenticationPrincipal CustomUserDetails currentUser) {
         def userDto = userMapper.toUserDto(userService.validateAndGetUserByUsername(currentUser.username))
         return ResponseEntity.ok(userDto)
+    }
+
+    @GetMapping
+    ResponseEntity<Map> getUsers(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        def pagingSort = PageRequest.of(page, size)
+        def pageResult = userService.getUsers(pagingSort)
+
+        def response = [:]
+
+        response["currentPage"] = pageResult.number
+        response["totalItems"] = pageResult.totalElements
+        response["itemsPerPage"] = pageResult.size
+        response["totalPages"] = pageResult.totalPages
+        response["users"] = pageResult.collect(userMapper::toUserDto)
+
+        return ResponseEntity.ok(response)
     }
 }
