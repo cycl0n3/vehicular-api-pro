1daeed087e061e17696916888d3e57c66bf24768
Milind <7milind7@gmail.com>
2023-04-23 23:36:28 +0530

    UserController is going V1

--- src/main/groovy/com/app/orderapi/rest/UserV1Controller.groovy
+++ src/main/groovy/com/app/orderapi/rest/UserV1Controller.groovy
@@ -1,7 +1,10 @@
 package com.app.orderapi.rest
 
+import com.app.orderapi.mapper.UserMapper
 import com.app.orderapi.rest.dto.UserDto
 import com.app.orderapi.security.CustomUserDetails
+import com.app.orderapi.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.GetMapping
@@ -12,8 +15,15 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/api/v1/users")
 class UserV1Controller {
 
+    @Autowired
+    UserService userService
+
+    @Autowired
+    UserMapper userMapper
+
     @GetMapping("/me")
     ResponseEntity<UserDto> getCurrentUser(@AuthenticationPrincipal CustomUserDetails currentUser) {
-        return ResponseEntity.ok(userMapper.toUserDto(userService.validateAndGetUserByUsername(currentUser.username)))
+        def userDto = userMapper.toUserDto(userService.validateAndGetUserByUsername(currentUser.username))
+        return ResponseEntity.ok(userDto)
     }
 }
